052f8abcac804857354715db58f83cee3aeceb5a
tilt287 <misasvinin49@gmail.com>
2025-12-05 11:25:51 +0300

    11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,27 +1,78 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 int main() {
 	setlocale(0, "");
-	int eznum;
-	cout << "[+] Программа - “Геометрические фигуры" << endl;
-	cout << "[1] Линия" << endl;
-	cin >> eznum;
-	if (eznum == 1) {
-		cout << "Линия" << endl;
-		cout << "[1] Горизонтальная" << endl;
-		cout << "[2] Вертикальная" << endl;
-	}
-	else {
-		cout << "Введено неправильно значение, повторите попытку" << endl;
-	}
-	int hardnum;
-	cin >> hardnum;
-	cout << "[+] Фигура: Линия" << endl;
-	cout << "[1] Горизонтальная" << endl;
-	cout << "[2] Вертикальная" << endl;
-	cout << "Выберете значение:" << endl;
+	cout << "[ + ] Программа - “Геометрические фигуры\n";
+	cout << "[1] Линия\n";
+	cout << "[2] Выход\n";
+	cout << "[+] Выберите фигуру : ";
+	int choice;
+	cin >> choice;
+	int choicetype;
+	bool exit = false;
+	do {
+		switch (choice) {
+		case 1: {
 
+			system("cls");
+			cout << "Фигура: 'Линия'\n";
+			cout << "[1] Горизонтальная\n";
+			cout << "[2] Вертикальная\n";
+			bool exitline = false;
+			do {
+				cin >> choicetype;
 
+				switch (choicetype) {
+				case 1: {
+					cout << "Длина линии: \n";
+					int length;
+					cin >> length;
+					cout << "Текстура линии: \n";
+					string texture;
+					cin >> texture;
+					cout << "Результат:\n";
+					while (length) {
+						cout << texture;
+						length -= 1;
+					}
+					exitline = true;
+					break;
+				}
+				case 2: {
+					cout << "Длина линии: \n";
+					int length;
+					cin >> length;
+					cout << "Текстура линии: \n";
+					string texture;
+					cin >> texture;
+					cout << "Результат:\n";
+					while (length) {
+						cout << texture << endl;
+						length -= 1;
+					}
+					exitline = true;
+					break;
+				}
+				default:
+					cout << "Нет такого значения! Повторите попытку\n";
+					break;
+				}
+
+			} while (!exitline);
+			break;
+		}
+		case 2: {
+			cout << "Выход из программы......\n";
+			exit = true;
+			break;
+
+		}
+		default: {
+			cout << "Нет такого значения! Повторите попытку\n";
+			break;
+
+		}
+		}
+	} while (!exit);
 	return 0;
 }
